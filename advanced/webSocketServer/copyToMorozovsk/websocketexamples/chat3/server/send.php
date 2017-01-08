e71acea036db8a72ff5967f95b49970e899a35ae
DESKTOP-V7305KN\кек <Kronos0041@gmail.com>
2017-01-08 14:36:47 +0300

    Add copy data for plugin

---
+++ advanced/webSocketServer/copyToMorozovsk/websocketexamples/chat3/server/send.php
@@ -0,0 +1,14 @@
+<?php
+//можно вызывать из своих скриптов
+//отправляет данные на вебсокет-сервер, который перенаправляет их на все клиенты
+
+$localsocket = 'tcp://127.0.0.1:8010';
+$message = 'test';
+
+$instance = stream_socket_client ($localsocket, $errno, $errstr);//соединямся с вебсокет-сервером
+
+fwrite($instance, json_encode(['message' => $message, 'userId' => 9441])  . "\n");//отправляем сообщение
+//fwrite($instance, json_encode(['message' => $message, 'clientId' => 68])  . "\n");//отправляем сообщение
+//fwrite($instance, json_encode(['message' => $message, 'PHPSESSID' => '4sk3sgqf1lqbjC2litl75db142'])  . "\n");//отправляем сообщение
+
+?>
